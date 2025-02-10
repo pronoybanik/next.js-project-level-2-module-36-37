4a585b9054390a28cc696358f398cb96eee22b32
PRONOY <pronoybanik82@gmail.com>
2025-02-10 18:40:53 +0600

    add blog section done

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,5 +1,31 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
 import React from "react";
+
+// set up and show frontend first 3 data in static  without loading
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return {
+    title: blog.title,
+    description: blog.description,
+  };
+}
 
 const BlogDetailsPage = async ({
   params,
